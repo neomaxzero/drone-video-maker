95e4339e9be440197b26a99664cee25a0aa1c865
Maxi <maximiliano.cespedes@coolblue.nl>
2020-10-16 21:56:56 +0200

    add more precius video settings

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log('Welcome to Drone video maker!')
+
+const FfmpegCommand = require('fluent-ffmpeg');
+
+// const command = ffmpeg();
+
+
+const command = ffmpeg('/sample/002 - 111022020.mov');
